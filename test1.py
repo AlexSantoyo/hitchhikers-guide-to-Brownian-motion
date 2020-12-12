0ae530e9dd6ae6646072e892b34a9143388c02f2
SkorohodAlex <ale.santoyo.c@gmail.com>
2020-12-12 15:33:50 -0600

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 12 15:27:38 2020
+
+@author: Alex y Fide
+"""
+
+"""
+The objective is to create a class for Brownian motion.
+The first use of this class is to generate sample paths of various processes related to BM, 
+for example:
+    - Non standard Brownian motion
+    - Brownian Drift
+    - Non intersecting Brownian motion
+    - Geometric Brownian motion
+    - Two dimensional Brownian motion
+    - Local time surface
+"""
