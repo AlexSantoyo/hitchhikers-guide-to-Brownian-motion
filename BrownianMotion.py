7e301de28f749d31f57676fccfec6df29f9c3063
Alex Santoyo <ale.santoyo.c@gmail.com>
2024-12-30 11:31:35 +0000

    Adding a TODO list. Adding example of expectation, variance and mgf.

--- BrownianMotion.py
+++ BrownianMotion.py
@@ -29,16 +29,31 @@ import random as rnd
 class BrownianMotion:
     """
     Arithmetic Brownian motion object.
-    Inputs:
-        mu: real number, the constant for the linear drift
-        sigma: real positive number, the standar deviation of the diffusion part
     """
-    def __init__(self,mu,sigma): #,time,No_buckets):
+    def __init__(self,mu,sigma,initp=0): #,time,No_buckets):
+        """
+        Args:
+            mu: real number, the constant for the linear drift
+            sigma: real positive number, the standar deviation of the diffusion part
+        
+        TODO:
+            - Define methods for Expectation, Variance, Moment Generating Function, Characteristic Function, etc...
+            - Change the sample_path(s) funciton(s) to a "simulate" method, to be generic and easily allow a generalization
+        """
         self.mu = mu
         self.sigma = sigma
-        self.initp = 0
+        self.initp = initp
         self.time = []
         self.N = []
+
+    def expectation(self, time):
+        return self.mu * time
+    
+    def variance(self, time):
+        return self.sigma * self.sigma * time
+    
+    def mgf(self,time):
+        return lambda u: np.exp( u * time * self.mu + u * u * time * time * self.sigma * self.sigma / 2 )
     
     def sample_path(self,initpoint,time,No_buckets):
         self.time = time
@@ -61,11 +76,11 @@ class BrownianMotion:
         self.dt = self.time/self.N
         #incrementos
         incr_mb = np.repeat(self.mu * self.dt,self.N) + np.sqrt(self.dt)*np.random.normal(0, self.sigma, size=(No_paths, self.N))
-        print(np.transpose(np.array([self.initp]*No_paths)).shape)
-        print(np.array([self.initp]*No_paths).shape)
-        print(incr_mb)
-        print(np.cumsum(incr_mb,axis=1))    
-        print(incr_mb.shape)   
+        # print(np.transpose(np.array([self.initp]*No_paths)).shape)
+        # print(np.array([self.initp]*No_paths).shape)
+        # print(incr_mb)
+        # print(np.cumsum(incr_mb,axis=1))    
+        # print(incr_mb.shape)   
 
         # sample paths
         return np.vstack((np.array([self.initp]*No_paths),np.transpose(np.cumsum(incr_mb, axis=1))))
@@ -80,10 +95,15 @@ class BrownianMotion:
         plt.show()
 
 if __name__ == "__main__":        
-    bm = BrownianMotion(0,1)
+    bm = BrownianMotion(-1,1)
     
+    print(bm.expectation(0.5))
+    print(bm.expectation(5))
+    print(bm.variance(0.5))
+    print(bm.variance(5))
+
     bmsp = bm.sample_paths(0,1,1000,8)        
-    print(bmsp.shape)
+    # print(bmsp.shape)
 
     bm.plot_sp(bmsp)        
             
